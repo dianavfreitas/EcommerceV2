7016b641b08e5ffea8537b3c24b465e5db2b2cde
dianavfreitas <dianavfreitass@gmail.com>
2024-07-23 22:05:39 -0400

    pushing progress

--- ecommercev2/src/Pages/Home.jsx
+++ ecommercev2/src/Pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
 import '../Pages/Home.css'
 import Rack from '../images/rack.jpg'
 import Dress from '../images/shopdresses.jpeg'
 import Tops from '../images/shoptops.jpeg'
 import Bottoms from '../images/shopbottoms.jpeg'
 import Jackets from  '../images/shopjackets.jpeg'
+import White from  '../images/white.jpg'
+import Green from '../images/green.jpg'
+import Blue from '../images/blue.jpg'
 import Club from '../images/joinget.png'
 import John from '../images/john.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 
 function Home() {
@@ -23,28 +30,36 @@ function Home() {
             <a href="/"><button>Shop Now</button></a>
         </div>
 
-        <div class="section2">
-            <img src={Dress} alt="dress" />
-
-            <img src={Tops} alt="tops" />
-
-            <img src={Bottoms} alt="bottoms" />
-        
-            <img src={Jackets} alt="jackets" />
+        <div class="john">
+          <h1>The joy of dressing is an art...</h1>
         </div>
-        <div class="buttons">
-            <a href="/"><button>Shop Dresses</button></a>
-            <a href="/"><button>Shop Tops</button></a>
-            <a href="/"><button>Shop Bottoms</button></a>
-            <a href="/"><button>Shop Jackets</button></a>
-        </div>
+
+
+        <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={Dress} alt="dress" /></SwiperSlide>
+        <SwiperSlide><img src={Tops} alt="tops" /></SwiperSlide>
+        <SwiperSlide><img src={Bottoms} alt="bottoms" /></SwiperSlide>
+        <SwiperSlide><img src={Jackets} alt="jackets" /></SwiperSlide>
+
+        <SwiperSlide><img src={White} alt="white" /></SwiperSlide>
+        <SwiperSlide><img src={Green} alt="green" /></SwiperSlide>
+        <SwiperSlide><img src={Blue} alt="blue" /></SwiperSlide>
+
+      </Swiper>
+
+      
 
         <div class="club">
             <a href="/"><img src={Club} alt="club" class="club"/></a>
-        </div>
-
-        <div class="john">
-            <img src={John} alt="john" class="john"/>
         </div>
     </main>
     
